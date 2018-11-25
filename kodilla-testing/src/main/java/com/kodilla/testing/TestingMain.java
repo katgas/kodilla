444d4dc9942aad39034ce2a17f5bc91bd86be9a7
katgas <mojepierwszekontorobocze@gmail.com>
2018-11-25 20:46:07 +0100

    New class TestingMain.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        double addition = calculator.addAToB(3,7);
+        double subtraction = calculator.subtractAFromB(5,20);
+        double division = calculator.divideAByB(40,5);
+        double multiplication = calculator.multiplyAByB(2,18);
+
+        if ((addition == 10) && (subtraction == 15)) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
+        if ((division == 8) && (multiplication == 36)) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public double addAToB(double a, double b) {
+        return a+b;
+    }
+    public double subtractAFromB(double a, double b) {
+        return b-a;
+    }
+    public double multiplyAByB(double a, double b) {
+        return a*b;
+    }
+    public double divideAByB(double a, double b) {
+        return a/b;
+    }
+
+}

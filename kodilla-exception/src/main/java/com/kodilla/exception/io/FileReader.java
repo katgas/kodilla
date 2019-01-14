b513aa8489680cfc2cff42e02875fa25c2432f5a
katgas <mojepierwszekontorobocze@gmail.com>
2019-01-14 23:08:40 +0100

    Task 8.2 - try-catch-finally.

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,31 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        //Path path = Paths.get(file.getPath());
+        //Path path = Paths.get("files/test.txt"); --IOException
+
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
+
+            fileLines.forEach(System.out::println);
+
+        } catch (IOException e) {
+
+            System.out.println("Oh no! Something went really wrong!" + e);
+
+        } finally {
+
+            System.out.println("I'm gonna always be there at the end.");
+        }
+
+        System.out.println(file.getPath());
+    }
+}
